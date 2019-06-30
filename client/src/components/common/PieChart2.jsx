04c2c8502e1193ca01a888dd9662574f206aa3ae
Charles Chiakwa <charleslukes28@gmail.com>
2019-06-30 22:49:14 +0100

    tidy up pie2 component

--- client/src/components/common/PieChart2.jsx
+++ client/src/components/common/PieChart2.jsx
@@ -25,9 +25,10 @@ function PieChart() {
         }),
       );
   }, []);
+
   return (
     <>
-      <Pie data={pieState.chartData} options={{}} />
+      <Pie data={pieState.chartData} options={{}} height={250} />
     </>
   );
 }
