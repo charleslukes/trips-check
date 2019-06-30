f986f9970560a3fe882b51cbf8fe1e7ca8d2333d
Charles Chiakwa <charleslukes28@gmail.com>
2019-06-30 22:53:01 +0100

    added master view to the link

--- client/src/components/driverD.jsx
+++ client/src/components/driverD.jsx
@@ -1,7 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import './driverD.css';
 
 function DriverDetails() {
-  return <>Hiiiiiii drivers</>;
+  const [driverNames, setDriverName] = useState([]);
+  const [driverInfo, setDriverInfo] = useState(0);
+
+  useEffect(() => {
+    fetch('/api/drivers')
+      .then(data => data.json())
+      .then(data => setDriverName(data.data));
+  }, []);
+
+  const handleClick = event => {
+    let displayValue;
+    const id = event.target.dataset.value;
+
+    driverNames.forEach(elem => {
+      if (elem.driverID === id) {
+        displayValue = elem;
+      }
+    });
+
+    setDriverInfo(displayValue);
+  };
+
+  return (
+    <>
+      <div className="showsDriver">
+        <div className="driversName">
+          {driverNames.map((elem, index) => (
+            <div key={index} className="driverPerson">
+              <p>{elem.name}</p>
+
+              <button onClick={e => handleClick(e)} data-value={elem.driverID}>
+                view
+              </button>
+            </div>
+          ))}
+        </div>
+
+        <div className="showsDetails">
+          {typeof driverInfo === 'object' && (
+            <div>
+              <h1>Driver Details</h1>
+              <div className="details">
+                <p>
+                  <span>Name:</span> {driverInfo.name}
+                </p>
+                <p>
+                  <span>Gender:</span> {driverInfo.gender}
+                </p>
+                <p>
+                  <span>Address:</span> {driverInfo.address}
+                </p>
+                <p>
+                  <span>Phone Num:</span> {driverInfo.phone}
+                </p>
+                <p>
+                  <span>Email:</span> {driverInfo.email}
+                </p>
+                <p>
+                  <span>ID:</span> {driverInfo.driverID}
+                </p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default DriverDetails;
