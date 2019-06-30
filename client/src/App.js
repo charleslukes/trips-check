50fd626de757f4e0bbb94d7ee473543056744eeb
Charles Chiakwa <charleslukes28@gmail.com>
2019-06-30 22:43:43 +0100

    include route link on app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/common/nav';
 // import DashBoard from './components/dashboard';
 import Main from './components/main';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Main />
     </div>
   );
